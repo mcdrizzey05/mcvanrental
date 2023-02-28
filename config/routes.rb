930331011f9b14257146cd8713f160e124ced4f6
Marie Claude <mc.leveille@gmail.com>
2023-02-28 11:36:46 +0100

    New basic homepagew

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: "offers#index"
+  resources :offers
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "users/:id", to: "users#show"
   # Defines the root path route ("/")
