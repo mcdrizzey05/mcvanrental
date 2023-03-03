d9cc88aac21466fd40fb26b2e4a4ef3afaddaf6f
Marie Claude <mc.leveille@gmail.com>
2023-03-03 12:05:53 +0100

    Add to_s to certain images

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-
-
 User.create(name: "Alina", email: "alina@alina.com", password: "123456")
 User.create(name: "MC", email: "mc@mc.com", password: "123456")
 User.create(name: "Andreia", email: "andreia@andreia.com", password: "123456")
