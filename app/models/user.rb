12785c4a8ab142e50851dacd611e556b7ef62236
Marie Claude <mc.leveille@gmail.com>
2023-03-03 11:18:39 +0100

    Added new method to filter bookings + styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  has_many :offers, dependent: :destroy
+         has_many :offers
+         has_many :bookings_as_owner, through: :offers, source: :bookings
+         has_many :bookings
 end
